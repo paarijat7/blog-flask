30c52fcef21677f8317a1dfb8e2cafc2ccb81bd8
paarijat7 <lokeshtmkc@gmail.com>
2022-09-28 01:03:05 +0530

    progress

--- blog.py
+++ blog.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [ {'author': 'paarijat thakur',
+        'title': 'blog1',
+        'date_posted':'22-12-22',
+        'content':'blog post1'},
+        {
+        'author': 'shiva',
+        'title': 'blog2',
+        'date_posted':'22-12-23',
+        'content':'blog post1'
+
+        }
+        ]
+
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About us")
 
 
 
